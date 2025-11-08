95558e6ad3ad2f1c6024790e53a0d93c52477182
Yiwei Ho <davidho0403@gmail.com>
2025-11-08 22:19:37 +0800

    Add ray-code extension
    - chore: screenshot
    - chore: remove hackathon org
    - chore: update description
    - fix: npm lock file miss
    - chore: add org
    - chore: add screenshot
    - fix: open extension preference action not working
    - chore: update get start view
    - chore: update ext logo
    - chore: get started command
    - chore: update ai instruction
    - feat: create file tool
    - refactor: remove unused workspace utility exports
    - feat: delete file tool
    - feat: apply edit tool
    - feat: search file tool
    - feat: workspace setting
    - feat: grep tool
    - feat: list dir tool
    - feat: read file tool
    - chore: init commit

---
+++ extensions/ray-code/src/tools/grep.ts
@@ -0,0 +1,21 @@
+import { readFile } from "node:fs/promises";
+import { resolveAndValidatePath } from "../utils/workspace";
+
+type Input = {
+  /**
+   * The relative path to the file from the workspace root
+   */
+  path: string;
+  /**
+   * The search query
+   */
+  query: string;
+};
+
+export default async function ({ path, query }: Input) {
+  const filePath = resolveAndValidatePath(path);
+  const content = await readFile(filePath, "utf8");
+  const matches = content.match(new RegExp(query, "g"));
+
+  return matches;
+}
